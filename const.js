a73d280f538b8ed5bf7b63e6f3d276ed2814de8c
qu_i <201885022@mail.dlut.edu.cn>
2021-05-14 10:08:05 +0800

    后端

---
+++ const.js
@@ -0,0 +1,29 @@
+const resUrl = "http://10.5.171.4:9000";
+const category = [
+  "Biomedicine",
+  "BusinessandManagement",
+  "ComputerScience",
+  "EarthSciences",
+  "Economics",
+  "Engineering",
+  "Education",
+  "Environment",
+  "Geography",
+  "History",
+  "Laws",
+  "LifeSciences",
+  "Literature",
+  "SocialSciences",
+  "MaterialsScience",
+  "Mathematics",
+  "MedicineAndPublicHealth",
+  "Philosophy",
+  "Physics",
+  "PoliticalScienceAndInternationalRelations",
+  "Psychology",
+  "Statistics",
+];
+module.exports = {
+  resUrl,
+  category,
+};
